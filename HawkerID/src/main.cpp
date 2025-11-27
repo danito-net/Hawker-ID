20120440656513e401203d39608ce21f122b0cdb
Danny Ismarianto Ruhiyat (danito) <info@danito.net>
2025-11-27 14:26:33 +0700

    feat: add WiFi + NTP time code for ESP32-S3

--- HawkerID/src/main.cpp
+++ HawkerID/src/main.cpp
@@ -1,18 +1,49 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include "time.h"
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid     = "CIKUR";
+const char* password = "cikur2023";
+
+// NTP config
+const char* ntpServer = "pool.ntp.org";
+// GMT+7 = 7 * 3600 = 25200 seconds
+const long  gmtOffset_sec = 7 * 3600;
+const int   daylightOffset_sec = 0;  // no DST in WIB
+
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  // Format: YYYY-MM-DD HH:mm:SS
+  char buf[25];
+  strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &timeinfo);
+  Serial.println(buf);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("WiFi connected, IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Configure NTP + timezone
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.println("Time synchronized, current WIB time:");
+  printLocalTime();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  printLocalTime();
+  delay(1000);
 }
